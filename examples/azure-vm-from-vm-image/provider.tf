59895a7405af20d4c1b94c3b6706af94f48fd700
sammy <sameera.madushanka@ifsworld.com>
2020-10-24 17:31:05 +0530

    Add configuration to create vm from vm image

---
+++ examples/azure-vm-from-vm-image/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+# Configure the Azure Provider
+provider "azurerm" {
+  features {}
+}
